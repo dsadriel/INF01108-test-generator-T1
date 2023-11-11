8bd11142b4fd2631f9c13ffba7fdf6186e5a63ac
Marcelo Basso <103913045+marcelobasso@users.noreply.github.com>
2023-11-11 20:20:02 -0300

    :bug: atualiza js code

--- Ramses/test-generator/test-generator.js
+++ Ramses/test-generator/test-generator.js
@@ -1,21 +1,21 @@
 const textarea = document.getElementById("file-content");
 const header = `TESTES RANDOMICOS - github: @marcelobasso\n.c\nCasos de teste - 2023-2\n.m\n65535\n.l\n.d\n.p\n0`;
-let step = Math.floor(Math.random() * 25);
 let n_tests = 50;
 var line;
 let string = [];
 
 textarea.textContent = header;
 for (let i = 0; i < n_tests; i++) {
+    let step = Math.floor(Math.random() * 25);
     let operation = Math.floor(Math.random() * 2) // 0/1;
     let word_size = Math.floor(Math.random() * 25); // 0-25 
     textarea.textContent += `\n.t\n.c\n` +
         `Teste ${i + 1}\n` +
         `.i\n` +
         `192=196\n` +
-        `193=221\n` +
+        `193=${196 + word_size + 1}\n` +
         `194=${step}\n` +
-        `195=${operation}`;
+        `195=${operation}\n`;
 
     for (let j = 0; j < word_size; j++) {
         string[j] =  Math.floor(Math.random() * (90 - 65 + 1) + 65); // 65-90
@@ -23,7 +23,7 @@ for (let i = 0; i < n_tests; i++) {
     }
 
     // convert string
-    string.map(char => {
+    string = string.map(char => {
         if (operation == 0) {
             char += step;
             char -= char > 90 ? 26 :0;
@@ -35,7 +35,7 @@ for (let i = 0; i < n_tests; i++) {
         return char;
     });
 
-    let end_second_string = 196 + word_size + 2;
+    let end_second_string = 196 + word_size + 1;
     textarea.textContent += `${end_second_string - 1}=0\n` +
         `.o\n`;
     
